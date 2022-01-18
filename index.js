2a43c4ffd42ed79a42311e221e4175cc6504cf06
Cezar <cezarguimaraes@gmail.com>
2022-01-18 16:05:17 -0300

    Requisito 5 concluído

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const { isEmailValid, isPasswordValid } = require('./validations/login-validatio
 const { isTokenValid, isNameValid,
   isAgeValid, isTalkValid } = require('./validations/createTalker-validation');
 
+const TALKERS_JSON = './talker.json';
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -17,12 +19,12 @@ app.get('/', (_request, response) => {
 });
 
 app.get('/talker', async (_req, res) => {
-  const talkers = await readContentFile('./talker.json');
+  const talkers = await readContentFile(TALKERS_JSON);
   res.status(200).json(talkers);
 });
 
 app.get('/talker/:id', async (req, res) => {
-    const talkers = await readContentFile('./talker.json');
+    const talkers = await readContentFile(TALKERS_JSON);
     const { id } = req.params;
     const talker = talkers.find((i) => i.id === parseInt(id, 10));
     
@@ -40,13 +42,29 @@ app.post('/login', isEmailValid, isPasswordValid, (req, res) => {
 app.post('/talker', isTokenValid, isNameValid, isAgeValid,
   isTalkValid, async (req, res) => {
     const { name, age, talk } = req.body;
-    const talkers = await readContentFile('./talker.json');
+    const talkers = await readContentFile(TALKERS_JSON);
     const newTalker = { id: talkers.length + 1, name, age, talk };
     
-    const newTalkers = await writeContentFile('./talker.json', newTalker);
+    const newTalkers = await writeContentFile(TALKERS_JSON, newTalker);
 
     res.status(201).json(newTalkers);
 });
+
+app.put('/talker/:id', isTokenValid, isNameValid, isAgeValid,
+  isTalkValid, async (req, res) => {
+    const { name, age, talk } = req.body;
+    const { id } = req.params;
+    const talkers = await readContentFile(TALKERS_JSON);
+    const talkerIndex = talkers.findIndex((t) => t.id === parseInt(id, 10));
+    const editedtalker = talkers[talkerIndex];
+    editedtalker.name = name;
+    editedtalker.age = age;
+    editedtalker.talk = talk;
+    talkers[talkerIndex] = editedtalker;
+    const newTalkers = await writeContentFile(TALKERS_JSON, talkers[talkerIndex]);
+
+    res.status(200).json(newTalkers);
+  });
 
 app.listen(PORT, () => {
   console.log(`Listening at the port ${PORT}`);
